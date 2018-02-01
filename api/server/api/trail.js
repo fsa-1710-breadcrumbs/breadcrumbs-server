051a905040f0c8bca69b13f8e2597d165fee5a57
Vanessa Jimenez <vjimenez127@gmail.com>
2018-02-01 14:06:11 -0500

    added trail routing

--- api/server/api/trail.js
+++ api/server/api/trail.js
@@ -3,19 +3,40 @@ const { User, Trail } = require('../db/models');
 
 module.exports = router;
 
-router.get('/', (req, res, next) => {
-  Trail.findAll()
-  .then(foundTrails => {
-    res.json(foundTrails);
+router.param('id', function (req, res, next, id) {
+  Trail.findById(id)
+  .then(trail => {
+    if (trail) {
+      req.trail = trail;
+      next();
+    } else {
+      const error = new Error('Trail does not exist!');
+      error.status = 404;
+      throw error;
+    }
   })
   .catch(next);
 });
 
+// GET api/trail
+router.get('/', (req, res, next) => {
+  Trail.findAll()
+  .then(foundTrails => res.json(foundTrails))
+  .catch(next);
+});
+
+// GET /api/trail/:id
+router.get('/:id', function (req, res, next) {
+  res.json(req.trail);
+});
+
+// POST /api/trail
 router.post('/', (req, res, next) => {
   Trail.create({
       where: {
+       origin: req.body.origin,
+       destination: req.body.destination,
        breadcrumbs: req.body.coords,
-       description: req.body.timestamp,
        userId: req.body.userId
     }
   })
@@ -23,4 +44,18 @@ router.post('/', (req, res, next) => {
       res.json(createdTrail);
     })
     .catch(next);
+});
+
+// PUT /api/trail/:id
+router.put('/:id', function (req, res, next) {
+  req.trail.update(req.body)
+  .then(trail => res.json(trail))
+  .catch(next);
+});
+
+// DELETE /api/trail/:id
+router.delete('/:id', function (req, res, next) {
+  req.trail.destroy()
+  .then(() => res.sendStatus(202))
+  .catch(next);
 });
